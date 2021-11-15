62b9534a9d663ebb5d262dc43448da935814dae2
Nicholas Alayra <nalayra@hotmail.com>
2021-11-14 16:47:03 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+const names = [];
+
+function writeCards(names, event) {
+    for (let i = 0; i < names.length; i++) {
+        names.splice(i, 1, (`Thank you, ${names[i]}, for the wonderful ${event} gift!`));
+        debugger;
+    }
+    return names;
+}
+
+writeCards(["Lisa", "Kaitlyn", "Jan"], "birthday")
+
+function countDown(integer) {
+    while (integer >= 0) {
+        console.log(integer);
+        integer--;
+    }
+}
